fb0dc12c8e089f91cbfacb6f4ff42b85c1dee5da
wrobell <wrobell@pld-linux.org>
2010-11-07 15:24:22 +0000

    - code cleanup

--- examples/tworects.py
+++ examples/tworects.py
@@ -21,13 +21,13 @@ import arouter
 
 router = arouter.Router()
 
-r1 = router.add(((100, 80), (120, 140)))
-r2 = router.add(((0, 100), (20, 120)))
-r3 = router.add(((200, 100), (220, 120)))
-r4 = router.add(((150, 130), (170, 160)))
+r1 = router.add(((0, 100), (20, 120)))
+r2 = router.add(((100, 80), (120, 140)))
+r3 = router.add(((150, 130), (170, 160)))
+r4 = router.add(((200, 100), (220, 120)))
 
 
-l = router.connect(r2, r3)
+l = router.connect(r1, r4)
 
 router.route()
 
